1a1e7162664e29dc066db8d56afc7b53ee4be081
heartbeatsfeeling <499954367@qq.com>
2015-06-13 18:23:47 +0800

    add timeout&&complete api
    增加超时和完成接口

--- uploadFile.js
+++ uploadFile.js
@@ -1,37 +1,51 @@
 //https://github.com/heartbeatsfeeling/uploadFile
 ;(function($) {
-	var uploadFile=function(config){
-		var url=config.url;
-		var target='target'+_uuid();
-		var $element=$(config.element);
-		var $form=$("<form>");
-		var $iframe=$("<iframe name='"+target+"'></iframe>");//set name method -- ie 7 bug
+	var uploadFile = function(config) {
+		var url = config.url;
+		var target = 'target' + _uuid();
+		var $element = $(config.element);
+		var $form = $("<form>");
+		var $iframe = $("<iframe name='" + target + "'></iframe>"); //set name method -- ie 7 bug
 		var filter = config.filter ? config.filter($element) : true;
-		var progress=config.progress||_noop;
-		var success=config.success||_noop;
-		uploadFile.success=function(data){
-			success(data)
+		var progress = config.progress || _noop;
+		var success = config.success || _noop;
+		var complete = config.complete || _noop;
+		var timeout = config.timeout;
+		var timeoutFn = null;
+		var _remove = function() {
 			$element.unwrap();
 			$iframe.remove();
 		};
+		if (timeout) {
+			timeoutFn = setTimeout(function() {
+				_remove();
+				complete('timeout', {});
+			}, timeout * 1000);
+		};
+		uploadFile.success = function(data) {
+			clearTimeout(timeoutFn);
+			complete('success', data);
+			success(data)
+			_remove();
+		};
 		$form.attr({
-			action:url,
-			target:target,
-			method:'post',
-			enctype:'multipart/form-data'
+			action: url,
+			target: target,
+			method: 'post',
+			enctype: 'multipart/form-data'
 		}).css({
-			width:'100%',
-			display:'inline'
+			width: '100%',
+			display: 'inline'
 		});
 		$iframe.css({
-			display:'none'
+			display: 'none'
 		});
-		if(filter){
+		if (filter) {
 			document.body.appendChild($iframe[0]);
 			$element.wrap($form);
-			progress($element,$form);
-			$("form[target="+target+"]").submit();
-		}else{
+			progress($element, $form);
+			$("form[target=" + target + "]").submit();
+		} else {
 
 		}
 	};
@@ -42,6 +56,6 @@
 	if (typeof exports !== 'undefined') {
 		module.exports = uploadFile;
 	} else {
-		window.uploadFile=uploadFile;
+		window.uploadFile = uploadFile;
 	};
 })(jQuery);
