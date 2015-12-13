78aaf4570ad8280905e2ee9de4778a35fad826f8
heartbeatsfeeling <499954367@qq.com>
2015-12-13 20:39:27 +0800

    更新api

--- uploadFile.js
+++ uploadFile.js
@@ -1,33 +1,46 @@
 //https://github.com/heartbeatsfeeling/uploadFile 
-;(function($) {
+;
+(function($) {
 	var uploadFile = function(config) {
 		var url = config.url;
 		var target = 'target' + _uuid();
 		var $element = $(config.element);
 		var $form = $("<form>");
-		var $iframe = $("<iframe name='" + target + "'></iframe>"); //set name method -- ie 7 bug
+		var $iframe = $("<iframe name='" + target + "'></iframe>");
 		var filter = config.filter ? config.filter($element) : true;
 		var progress = config.progress || _noop;
 		var success = config.success || _noop;
 		var complete = config.complete || _noop;
 		var timeout = config.timeout;
-		var timeoutFn = null;
+		var timeoutFn = config.timeoutFn || _noop;
+		var html5Mode = config.html5Mode;
+		var formData;
+		var xhr = new XMLHttpRequest();
 		var _remove = function() {
 			$element.unwrap();
 			$iframe.remove();
 		};
-		if (timeout) {
-			timeoutFn = setTimeout(function() {
-				_remove();
-				complete('timeout', {});
-			}, timeout * 1000);
-		};
-		uploadFile.success = function(data) {
-			clearTimeout(timeoutFn);
-			complete('success', data);
-			success(data)
+		var _success = function(data) {
+			clearTimeout(timer);
+			complete($element);
+			success(data, $element);
 			_remove();
 		};
+		var _parseData = function(data) {
+			try {
+				data = $.parseJSON(data);
+			} catch (err) {
+
+			};
+			return data;
+		};
+		var timer = setTimeout(function() {
+			timeoutFn($element);
+			_remove();
+			complete($element);
+			xhr.onreadystatechange = null;
+			$iframe.unload('load');
+		}, timeout || 1000 * 20);
 		$form.attr({
 			action: url,
 			target: target,
@@ -41,13 +54,44 @@
 			display: 'none'
 		});
 		if (filter) {
-			document.body.appendChild($iframe[0]);
 			$element.wrap($form);
-			progress($element, $form);
-			$("form[target=" + target + "]").submit();
-		} else {
+			if (!!window.FormData && html5Mode) {
+				formData = new FormData($("form[target='" + target + "']")[0]);
 
-		}
+				xhr.upload.addEventListener("progress", function(evt) { // progress
+					var data = {};
+					data.html5Mode = html5Mode;
+					data.loaded = evt.loaded;
+					data.total = evt.total;
+					progress(data, $element)
+				}, false);
+				xhr.onreadystatechange = function(e) { //complete
+					var data = '';
+					if (xhr.readyState == 4) {
+						if (xhr.status == 200) {
+							data = _parseData(xhr.responseText);
+							_success(data);
+						} else {
+							_remove();
+							complete($element);
+						};
+					};
+				};
+				// start upload
+				xhr.open("POST", url, true);
+				xhr.send(formData);
+			} else {
+				document.body.appendChild($iframe[0]);
+				progress({}, $element);
+				$("form[target=" + target + "]").submit();
+				$iframe.bind('load', function() {
+					var $this = $(this);
+					var data = $this.contents().find('body').text();
+					data = _parseData(data);
+					_success(data);
+				});
+			};
+		} else {}
 	};
 	var _noop = function() {};
 	var _uuid = function() {
