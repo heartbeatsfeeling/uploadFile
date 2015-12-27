96dd43a7b000a675b2eaf8f332fbe9f2df78aa29
heartbeatsfeeling <499954367@qq.com>
2015-12-27 17:46:34 +0800

    new api

--- example/index.js
+++ example/index.js
@@ -11,23 +11,6 @@ app.get('/uploadFile', function(req, res) {
 	res.sendfile('./static/index.html');
 });
 app.post('/file', function(req, res) {
-	var fileName = (req.files.file.path).split('\\').pop();
-	var type = " " + fileName.split('.').pop() + " ";
-	var filterType = ' png gif jpg ';
-	var data=null;
-	if (filterType.indexOf(type) != -1) {
-		data = {
-			flag: true,
-			id: 1, //测试使用
-			path: 'http://127.0.0.1:3000/file/' + fileName
-		};
-	} else {
-		data={
-			flag:false,
-			id:'',
-			path:''
-		}
-	};
-	res.end("<body onload=" + "parent.uploadFile.success(" + JSON.stringify(data) + ")></body>");
+	res.end('{"id":"1","src":"id.png"}');
 });
 app.listen('3000');
---
+++ example/static/js/jquery.uploadFile.js
@@ -0,0 +1,108 @@
+//https://github.com/heartbeatsfeeling/uploadFile 
+;
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory(require('jquery'));
+	} else {
+		factory(root.jQuery);
+	}
+}(window, function($) {
+	$.fn.uploadFile = function(config) {
+		var url = config.url;
+		var target = 'target' + _uuid();
+		var $element = $(this);
+		var $form = $("<form>");
+		var $iframe = $("<iframe name='" + target + "'></iframe>");
+		var filter = config.filter ? config.filter($element) : true;
+		var progress = config.progress || _noop;
+		var success = config.success || _noop;
+		var complete = config.complete || _noop;
+		var timeout = config.timeout;
+		var timeoutFn = config.timeoutFn || _noop;
+		var html5Mode = config.html5Mode;
+		var formData;
+		var xhr = new XMLHttpRequest();
+		var _remove = function() {
+			$element.unwrap();
+			$iframe.remove();
+		};
+		var _success = function(data) {
+			clearTimeout(timer);
+			complete($element);
+			success(data, $element);
+			_remove();
+		};
+		var _parseData = function(data) {
+			try {
+				data = $.parseJSON(data);
+			} catch (err) {
+
+			};
+			return data;
+		};
+		var timer = setTimeout(function() {
+			timeoutFn($element);
+			_remove();
+			complete($element);
+			xhr.onreadystatechange = null;
+			$iframe.unload('load');
+		}, timeout || 1000 * 20);
+		$form.attr({
+			action: url,
+			target: target,
+			method: 'post',
+			enctype: 'multipart/form-data'
+		}).css({
+			width: '100%',
+			display: 'inline'
+		});
+		$iframe.css({
+			display: 'none'
+		});
+		if (filter) {
+			$element.wrap($form);
+			if (!!window.FormData && html5Mode) {
+				formData = new FormData($("form[target='" + target + "']")[0]);
+
+				xhr.upload.addEventListener("progress", function(evt) { // progress
+					var data = {};
+					data.html5Mode = html5Mode;
+					data.loaded = evt.loaded;
+					data.total = evt.total;
+					progress(data, $element)
+				}, false);
+				xhr.onreadystatechange = function(e) { //complete
+					var data = '';
+					if (xhr.readyState == 4) {
+						if (xhr.status == 200) {
+							data = _parseData(xhr.responseText);
+							_success(data);
+						} else {
+							_remove();
+							complete($element);
+						};
+					};
+				};
+				// start upload
+				xhr.open("POST", url, true);
+				xhr.send(formData);
+			} else {
+				document.body.appendChild($iframe[0]);
+				progress({}, $element);
+				$("form[target=" + target + "]").submit();
+				$iframe.bind('load', function() {
+					var $this = $(this);
+					var data = $this.contents().find('body').text();
+					data = _parseData(data);
+					_success(data);
+				});
+			};
+		} else {}
+	};
+	var _noop = function() {};
+	var _uuid = function() {
+		return (+new Date).toString() + Math.random().toString(16).substr(2, 8);
+	};
+}));
